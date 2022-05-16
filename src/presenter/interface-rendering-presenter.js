011857e3659779348af309daa03f478ce810a107
Влад <v4nkov.v@yandex.ru>
2022-05-16 18:09:51 +0300

    Добавил реализацию подпункта 1.2 тех.задания, за исключением 'изменения фильтра или сортировки сбрасывает счетчик паказа фильмов и отсчет начинается заново.'

--- src/presenter/interface-rendering-presenter.js
+++ src/presenter/interface-rendering-presenter.js
@@ -5,14 +5,18 @@ import CardMovieView from '../view/card-movie-view.js';
 import ShowMoreButtonView from '../view/show-more-button-view.js';
 import FilmContainerView from '../view/film-container-view.js';
 import PopupForInformationView from '../view/popup-for-information-view.js';
+import NoTaskView from '../view/no-task-view.js';
 import {render} from '../render.js';
 
+const numberOfDrawnFilmsPerTime = 5;
 
 export default class InterfaceRenderingPresenter {
 
   init = (renderingContainer,generatorObject) => {
 
+    let counterOfNumberOfRenderedFilms = numberOfDrawnFilmsPerTime;
     this.containerForDescribingMovieCard = [...generatorObject.getFilms()];
+    const movieCards = this.containerForDescribingMovieCard;
 
     const headerLogo = document.querySelector('.header');
     render(new UserTitleView(), headerLogo);
@@ -23,13 +27,34 @@ export default class InterfaceRenderingPresenter {
     render(new FilmContainerView(), renderingContainer);
 
     const filmsList = document.querySelector('.films-list');
-    render(new ShowMoreButtonView(), filmsList);
+    this.showMoreButtonView = new ShowMoreButtonView();
 
     const filmsListContainer = document.querySelector('.films-list__container');
     const body = document.querySelector('body');
 
-    for(let i=0; i<this.containerForDescribingMovieCard.length; i++){
-      callPopup(this.containerForDescribingMovieCard[i]);
+    if(this.containerForDescribingMovieCard.every((value) => value === 0)){
+      render(new NoTaskView(), filmsList);
+    }else{
+      for(let i=0; i<Math.min(this.containerForDescribingMovieCard.length, numberOfDrawnFilmsPerTime); i++){
+        callPopup(this.containerForDescribingMovieCard[i]);
+      }
+    }
+
+    if(this.containerForDescribingMovieCard.length > numberOfDrawnFilmsPerTime){
+      render(this.showMoreButtonView,filmsList);
+      const showMore = document.querySelector('.films-list__show-more');
+      showMore.addEventListener('click', showMovieCards);
+    }
+
+    function showMovieCards (){
+      movieCards
+        .slice(counterOfNumberOfRenderedFilms, counterOfNumberOfRenderedFilms + numberOfDrawnFilmsPerTime)
+        .forEach((value)=>{callPopup(value);});
+      counterOfNumberOfRenderedFilms += numberOfDrawnFilmsPerTime;
+      if(counterOfNumberOfRenderedFilms >= movieCards.length){
+        const oldShowMoreButton = document.querySelector('.films-list__show-more');
+        filmsList.removeChild(oldShowMoreButton);
+      }
     }
 
     function callPopup (films){
